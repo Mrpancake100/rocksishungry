5824e8f49cbcd949f645af46799f100721bcbd69
Rocky "Rocks" TheDog <96673355+AdognamedRocky@users.noreply.github.com>
2026-05-30 20:24:08 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -11,20 +11,20 @@ const siteConfig = {
       icon: "instagram",
       url: "https://instagram.com/mrrockythedog"
     },
-    {
-      label: "X",
-      icon: "x",
-      url: "https://x.com/SmackerTheDog"
-    },
+   {
+  label: "Steam",
+  icon: "steam",
+  url: "https://steamcommunity.com/profiles/76561199503565317/"
+},
  {
   label: "TikTok",
   icon: "tiktok",
   url: "https://tiktok.com/@SmackerTheDog"
 },
 {
-  label: "YouTube",
-  icon: "youtube",
-  url: "https://youtube.com/@rockythedog360"
+  label: "Furtrack",
+  image: "assets/furtrack-transparent-white.png",
+  url: "https://www.furtrack.com/user/RockyTheDog/uploads"
 },
   ],
 contacts: [
